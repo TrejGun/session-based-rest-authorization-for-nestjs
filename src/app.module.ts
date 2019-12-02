f4ad5aa771f017571ee185f825e4492eb8c74eca
TrejGun <trejgun@gmail.com>
2019-12-02 22:12:33 +0200

    use option factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {Module} from "@nestjs/common";
 import {AuthModule} from "./auth/auth.module";
 import {UserModule} from "./user/user.module";
-import ormconfig from "./ormconfig";
+import {TypeOrmConfigService} from "./typeorm.options";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), AuthModule, UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
